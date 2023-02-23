ec177496ead648be7150412b425d90d8b32c4e8e
Daryna Lobkova <darinka021193@gmail.com>
2023-02-23 09:27:39 +0100

    not working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-const validator = require('validator');
-const throttle = require('lodash.throttle');
+import * as basicLightbox from 'basiclightbox';
+const basicLightbox = require('basiclightbox');
+import 'basiclightbox/dist/basicLightbox.min.css'
+import './sass/styles.scss';
+
+const openModalBtnRef = document.querySelector('button[data-open-modal]');
+
+
+const modalTemplate = document.querySelector('#modal');
+const instance = basicLightbox.create(modalTemplate);
+console.log(instanse);
+
+
+openModalBtnRef.addEventListener('click', () => { instance.show()})
+
+
+
+
 
 
